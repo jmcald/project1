28cca457765ad442fea4e851f39471f5ee642285
RyanJF <ryanjfreeman77@gmail.com>
2018-09-27 19:50:44 -0500

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ var photos = true;
 var verifiable = true;
 
 var dumStartDate = "06/25/2018";
-var dumEndDate = "07/04/2018";
+var dumEndDate = "08/04/2018";
 
 // getting trip info from dom
 // var location = $("#destination").val().trim();
@@ -20,26 +20,27 @@ var dumEndDate = "07/04/2018";
 var returnDays = (startD, endD) => {
     var sDate = moment(startD);
     var eDate = moment(endD);
-    
+
     while (sDate <= eDate) {
-       var day =  moment(sDate).format("D");
-       days.push(day);
-       sDate = moment(sDate).add(1, "d");
-       console.log(days);
-    }  
-    
+        var day = moment(sDate).format("D");
+        days.push(day);
+        sDate = moment(sDate).add(1, "d");
+    }
+    console.log(days);
+
 };
 var returnMonths = (startD, endD) => {
-    var sDate = moment(startD);
-    var eDate = moment(endD);
-    
-    while (sDate <= eDate) {
-       var month =  moment(sDate).format("M");
-       months.push(month);
-       sDate = moment(sDate).add(1, "m");
-       console.log(months);
-    }  
-    
+    var sMonth = moment(startD);
+    var eMonth = moment(endD);
+
+    while (sMonth <= eMonth) {
+        var month = moment(sMonth).format("M");
+        months.push(month);
+        sMonth = moment(sMonth).add(1, "M");
+        console.log(sMonth);
+    }
+    console.log(months);
+
 };
 returnDays(dumStartDate, dumEndDate);
 returnMonths(dumStartDate, dumEndDate);
@@ -64,13 +65,13 @@ function calliNatAPI() {
         var length = res.total_results;
         var name;
         var img;
-        
+
     });
 }
 
 
-$(document).ready(function() {
-    $("#btn-submit").on("click", function(event) {
+$(document).ready(function () {
+    $("#btn-submit").on("click", function (event) {
         event.preventDefault();
     });
 });
